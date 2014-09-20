02966b1bd276258f2ef20978805ade555c509ff3
Craig Horsborough <craighorsborough@Craigs-MacBook-Pro.local>
2014-09-20 17:31:01 +0100

    Added arrive, depart, train_count methods

--- lib/station.rb
+++ lib/station.rb
@@ -1,9 +1,23 @@
 class Station
 
 
-	def empty?
-		true
+	def initialize
+		@trains = []
+	end
+	
+	def arrive(train)
+		raise "Already a train in station" if train_count == 1
+		@trains << train
 	end
 
+	def train_count
+		@trains.count
+	end
+
+	def depart(train)
+		@trains.delete(train)
+	end
+
+	
 
 end
