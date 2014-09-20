77658d7126b61079be1228316f08dd1db466f855
Craig Horsborough <craighorsborough@Craigs-MacBook-Pro.local>
2014-09-20 17:27:35 +0100

    Added arrive, depart & full tests

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,10 +1,29 @@
 require "station"
+require "train"
 
 describe Station do
+
+	let (:station) {Station.new}
+	let (:train) {Train.new}
 	
 	it "should be empty after we create it" do
-		station = Station.new
-		expect(station).to be_empty
+		expect(station.train_count).to eq(0)
+	end
+
+	it "Should let trains arrive at station" do 
+		station.arrive(train)
+		expect(station.train_count).to eq(1)
+	end
+
+	it "Should let trains leave the station" do
+		station.arrive(train)
+		station.depart(train)
+		expect(station.train_count).to eq(0)
+	end
+
+	it "should only hold 1 train" do
+		station.arrive(train)
+		expect { station.arrive(train) }.to raise_error("Already a train in station")
 	end
 
 end
