f6eb99113a79d6b0d7721fae8547be2277b6d666
Craig Horsborough <craighorsborough@Craigs-MacBook-Pro.local>
2014-09-21 17:29:45 +0100

    refactored station spec

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,18 +1,14 @@
 require "station"
 require "train"
 require "passenger"
-require "money"
 
 describe Station do
 
 	let (:station) {Station.new}
 	let (:train) {Train.new}
-	let (:passenger) {Passenger.new}
-	let (:money) {Money.new}
 	
 	it "should be empty after we create it" do
 		expect(station.train_count).to eq(0)
-		expect(station.passenger_count).to eq(0)
 	end
 
 	it "Should let trains arrive & depart station" do 
@@ -26,17 +22,4 @@ describe Station do
 		station.arrive(train)
 		expect { station.arrive(train) }.to raise_error("Already a train in station")
 	end
-
-	it "Should allow passengers on & off" do 
-		expect(station.passenger_count).to eq(0)
-		station.touch_on(passenger)
-		expect(station.passenger_count).to eq(1)
-		station.touch_off(passenger)
-		expect(station.passenger_count).to eq(0)
-	end
-
-	#it "Should not let passengers onto station if have less that £2" do
-	#	passenger.no_money(money)
-	#	expect{station.touch_on(passenger)}.to raise_error("Not enough money")
-	#end
 end
