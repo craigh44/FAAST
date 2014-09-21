c9e7be47a18f50e6c75c2fdc040c4d2ae7a2f707
Craig Horsborough <craighorsborough@Craigs-MacBook-Pro.local>
2014-09-21 17:32:19 +0100

    Added release test

--- spec/passenger_holder_spec.rb
+++ spec/passenger_holder_spec.rb
@@ -9,10 +9,15 @@ let(:holder) {PersonHolder.new}
 let(:passenger) {Passenger.new}
 
 
-it "should be able to accept passengers" do 
+it "should be able to accept and release passengers" do 
 	holder.accept(passenger)
 	expect(holder.passenger_count).to eq(1)
+	holder.release(passenger)
+	expect(holder.passenger_count).to eq(0)
 end
+
+	
+
 
 
 
