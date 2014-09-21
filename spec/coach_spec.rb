94c34c5d0778deb24fbb124d1253bb2c213106ee
Craig Horsborough <craighorsborough@gmail.com>
2014-09-21 21:53:00 +0100

    Added default capacity & raise error

--- spec/coach_spec.rb
+++ spec/coach_spec.rb
@@ -5,13 +5,20 @@ require "train"
  
 describe Coach do 
 
-include PassengerHolder
+let(:coach) {Coach.new(:capacity => 40)}
+let(:passenger) {Passenger.new}
 
+	def fill_coach
+		40.times {coach.accept(passenger)}
+	end
 
+	it "Should be able to set default capacity on initilization" do 
+		expect(coach.capacity).to eq(40)
+	end
 
-	#it "Should raise error if coach full" do 
-	#	40.times {coach.accept(passenger)}
-	#	expect {coach.accept(passenger)}.to raise_error("Coach is full")
-	#end
+	it "Should raise error if coach full" do 
+		fill_coach
+		expect {coach.accept(passenger)}.to raise_error("Coach is full")
+	end
 
 end
