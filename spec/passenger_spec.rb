57dc49d95715f27ef150e7a88fd2f419b606a139
Craig Horsborough <craighorsborough@gmail.com>
2014-09-21 21:56:23 +0100

    Added balance

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,12 +1,12 @@
-
+require "passenger"
 
 describe Passenger do 
 
+	let(:passenger) {Passenger.new(:balance => 10)}
 
-
-
-
-
+it "Should have a balance when created" do
+	expect(passenger.balance).to eq(10) 
+end
 
 
 end
