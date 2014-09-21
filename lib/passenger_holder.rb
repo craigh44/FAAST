d2efc8207a425a607199b3112903c6e006085131
Craig Horsborough <craighorsborough@gmail.com>
2014-09-21 21:47:49 +0100

    Added capacity and full? methods

--- lib/passenger_holder.rb
+++ lib/passenger_holder.rb
@@ -1,11 +1,21 @@
 module PassengerHolder
 
+	DEFAULT_CAPACITY = 10
+
+	def capacity
+		@capacity ||= DEFAULT_CAPACITY
+	end
+
+	def capacity=(value)
+		@capacity = value
+	end
+
 	def initialize
 		@passengers = []
 	end
 
 	def accept(passenger)
-
+		raise "Coach is full" if full?
 		@passengers << passenger
 	end	 
 
@@ -15,5 +25,9 @@ module PassengerHolder
 
 	def passenger_count
 		@passengers.count
+	end
+
+	def full?
+		passenger_count == capacity
 	end
 end
