86510bbc0513a21b60af369878413f1ad173531a
Craig Horsborough <craighorsborough@Craigs-MacBook-Pro.local>
2014-09-21 17:29:03 +0100

    Added passenger holder spec

---
+++ spec/passenger_holder_spec.rb
@@ -0,0 +1,21 @@
+require "passenger_holder"	
+require "passenger"
+
+class PersonHolder; include PassengerHolder; end
+
+describe PassengerHolder do 
+
+let(:holder) {PersonHolder.new}
+let(:passenger) {Passenger.new}
+
+
+it "should be able to accept passengers" do 
+	holder.accept(passenger)
+	expect(holder.passenger_count).to eq(1)
+end
+
+
+
+
+
+end
