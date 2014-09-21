6eb8a3deb4ca58d50f865eae15a0a6e638ccf635
Craig Horsborough <craighorsborough@Craigs-MacBook-Pro.local>
2014-09-21 17:26:41 +0100

    Refactored & added passenger holder

--- lib/coach.rb
+++ lib/coach.rb
@@ -1,19 +1,10 @@
+require_relative "passenger_holder"
+
 class Coach
+
+	include PassengerHolder
 
 	def initialize
 		@passengers = []
-	end 
-
-	def board(passenger)
-		raise "Coach is full" if passenger_count == 40
-		@passengers << passenger
-	end
-
-	def get_off(passenger)
-		@passengers.delete(passenger)
-	end
-
-	def passenger_count
-		@passengers.count
 	end
 end 
