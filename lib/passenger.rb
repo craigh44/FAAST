95a5df531c0fad459a811b20eda714000584e50a
Craig Horsborough <craighorsborough@gmail.com>
2014-09-21 21:44:08 +0100

    Added balance methods

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,5 +1,21 @@
 class Passenger
 
-	
+DEFAULT_BALANCE = 10
 
+	def initialize(options = {})
+		self.balance = options.fetch(:balance, balance)
+		@balance = []
+	end
+
+	def balance
+		@balance = DEFAULT_BALANCE
+	end
+
+	def balance=(value)
+		@balance = value
+	end
+
+	def balance_count
+		@balance.count
+	end
 end
