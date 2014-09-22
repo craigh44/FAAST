6414b3087c731999945bb2026107048527505ddb
Craig Horsborough <craighorsborough@gmail.com>
2014-09-22 20:05:04 +0100

    Added touch in and minus balance tests

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,12 +1,22 @@
 require "passenger"
+require "station"
 
 describe Passenger do 
 
-	let(:passenger) {Passenger.new(:balance => 10)}
+	let(:passenger) {Passenger.new}
+	let(:station) { double :station}
 
 it "Should have a balance when created" do
 	expect(passenger.balance).to eq(10) 
 end
 
+ it "Should -2 from balance when passenger touches in" do 
+ 	passenger.touch_in
+ 	expect(passenger.balance).to eq(8)
+ end
 
+  it "should not be able to touch in if balance less than 2" do
+  	4.times {passenger.touch_in}
+   	expect{passenger.touch_in}.to raise_error("No funds")
+   end
 end
