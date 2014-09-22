a6e71a28764274aa2f7717df5984b360a4a49348
Craig Horsborough <craighorsborough@gmail.com>
2014-09-22 20:04:26 +0100

    Refactored code

--- spec/passenger_holder_spec.rb
+++ spec/passenger_holder_spec.rb
@@ -9,9 +9,12 @@ let(:holder) {PersonHolder.new}
 let(:passenger) {Passenger.new}
 
 
-it "should be able to accept and release passengers" do 
+it "should be able to accept passengers" do 
 	holder.accept(passenger)
 	expect(holder.passenger_count).to eq(1)
+end
+
+	it "should be able to release passengers" do
 	holder.release(passenger)
 	expect(holder.passenger_count).to eq(0)
 end
